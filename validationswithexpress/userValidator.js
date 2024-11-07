6c9cfda2a0c7a70f80f522adf820413c2b02697f
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-11-07 16:01:08 +0200

    new changes

--- validationswithexpress/userValidator.js
+++ validationswithexpress/userValidator.js
@@ -7,9 +7,9 @@ const userValidator = {
     // body('birthdate').isISO8601().withMessage('Valid birthdate is required'),
     body('phoneNumber')
         .notEmpty().withMessage(' برجاء ادخل رقم الهاتف')
-        .isLength({ min: 11, max:11 }).withMessage('رقم الهاتف يجب أن يتكون من 10 أرقام على الأقل'),
+        .isLength({ min: 11, max:11 }).withMessage('رقم الهاتف يجب أن يتكون من 11 رقم على الأقل'),
     body('password').isLength({ min: 6 }).withMessage(' برجاء ادخال الرقم السري'),
-    body('idNumber').notEmpty().withMessage('برجاء ادخال الرقم القومي').isLength({ min: 14, max:14 }).withMessage('رقم القومى يجب أن يتكون من 14 أرقام على الأقل'),
+    body('idNumber').notEmpty().withMessage('برجاء ادخال الرقم القومي').isLength({ min: 14, max:14 }).withMessage('رقم القومى يجب أن يتكون من 14 رقم على الأقل'),
     // body('companyname').notEmpty().withMessage('Company name is required'),
     // body('adress').notEmpty().withMessage('Address is required'),
     // body('specialist').notEmpty().withMessage('Specialist field is required')

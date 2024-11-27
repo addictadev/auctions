df997f3be7ca942ec1a6564b47b0bc45da4f2d9c
M-Metaw3 <mahmoudmetawea161@gmail.com>
2024-11-27 21:36:54 +0200

    شششششششششش

--- controllers/paymentcontroller/processPayment .js
+++ controllers/paymentcontroller/processPayment .js
@@ -288,7 +288,9 @@ const processPayment = async (req, res, next) => {
 
     const adminNotificationMessage = billingMethod === 'wallet'
       ? `تمت معالجة الدفع بمبلغ ${dueAmount} بنجاح للبند ${item.name} بواسطة المستخدم ${user?.phoneNumber}.`
-      : `تم تقديم طلب دفع للبند ${item.name} ويتطلب موافقة الإدارة.`;
+      // : ` تم تقديم طلب دفع للبند ${item.name} من المستخدم   ويتطلب موافقة الإدارة.`;
+      : ` تمت معاجلة الدفع بمبلغ ${dueAmount} بنجاح للبند ${item.name} بواسطة المستخدم : ${user?.phoneNumber}`;
+
       
     const adminNotification = new AdminNotification({
       user,
